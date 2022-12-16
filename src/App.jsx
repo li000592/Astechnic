9ba359de1d430b696d98723c6223c3ea847cf0fc
li000592 <li000592@algonquinlive.com>
2022-12-16 15:17:14 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Landing />}></Route>
+        {/* <Route path="/" element={<Landing />}></Route> */}
         <Route path="/resources" element={<Landing />}></Route>
         <Route path="/products" element={<Landing />}></Route>
         <Route path="/contacts" element={<Landing />}></Route>
