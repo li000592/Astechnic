48f15bc21ccc5ee5a6b85b6628d77ec344884266
li000592 <li000592@algonquinlive.com>
2022-12-16 15:20:46 -0500

    test

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -3,12 +3,12 @@ import CarouselSection from "./carousel/CarouselSection";
 import IndustriesIntro from "./industries_intro/IndustriesIntro";
 import IntroTwo from "./intro_2/IntroTwo";
 import IntroContact from "./intro_contact/IntroContact";
-import MainBoard from "./mainboard/mainBoard";
+// import MainBoard from "./mainboard/mainBoard";
 
 export default function Landing() {
   return (
     <div className="landing-container">
-      <MainBoard />
+      {/* <MainBoard /> */}
       <BreifIntro />
       <IndustriesIntro />
       <CarouselSection />
