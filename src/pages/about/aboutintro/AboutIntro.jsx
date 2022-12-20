cda6d4d1987e80be88fc7c5185c0182004f5931f
Haorong <li000592@algonquinlive.com>
2022-12-19 22:19:22 -0500

    about page: 1. add basic mainboard 2. add basic main intro 3. add learn more

---
+++ src/pages/about/aboutintro/AboutIntro.jsx
@@ -0,0 +1,22 @@
+export default function AboutIntro() {
+  return (
+    <>
+      <section className="about-intro-container">
+        <div className="intro-left">
+          <div className="intro-title">How we deliver</div>
+          <div className="intro-body">
+            OnPoint delivers results in a variety of industrial domains around
+            the globe. The key to our proven performance is our unique
+            combination of data-driven digital expertise and real-world,
+            in-the-field industry experience. As part of Koch Engineered
+            Solutions (KES), we bring a long history in refining and
+            petrochemical markets, emission management, water treatment and
+            other industrial applications to every project. We understand your
+            industry, your challenges, and opportunities.
+          </div>
+        </div>
+      </section>
+      <div className="blue-br"></div>
+    </>
+  );
+}
---
+++ src/pages/about/aboutmainboard/AboutMainBoard.jsx
@@ -0,0 +1,30 @@
+import { Suspense } from "react";
+import mainVideo from "/src/assets/videos/about_video.mp4";
+import "./AboutMainBoard.css"
+export default function AboutMainBoard() {
+
+    return (
+        <div id="about-video-container">
+            <div className="green-brick-containerr">
+                <div className="green-brick-triangle" />
+            </div>
+            <Suspense fallback={<>loading...</>}>
+
+                <video autoPlay muted loop id="main-board-video">
+                    <source src={mainVideo} type="video/mp4" />
+                </video>
+
+            </Suspense>
+            <div className="about-main-content">
+                <h1 className="about-video-title animate__animated animate__fadeInDown blue">DIGITAL<br />
+                    SOLUTIONS,</h1>
+                <h1 className="about-video-title animate__animated animate__fadeInDown">REAL-WORLD<br />RESULTS.</h1>
+            </div>
+            <div className="green-brick-container animate__animated animate__fadeInRight">
+                <div className="green-brick"></div>
+                <div className="green-brick-triangle" />
+            </div>
+
+        </div>
+    )
+}
