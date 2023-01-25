bfc47256e9d53223091608dec8baabf5f1dec79c
li000592 <li000592@algonquinlive.com>
2023-01-25 18:15:16 -0500

    update video tab

--- src/pages/landing/mainboard/MainBoard.jsx
+++ src/pages/landing/mainboard/MainBoard.jsx
@@ -15,7 +15,7 @@ export default function MainBoard() {
     <section id="video-container">
       <div className="video-filter"></div>
       <Suspense fallback={<>loading...</>}>
-        <video autoPlay muted loop id="main-board-video">
+        <video controls autoplay loop muted playsinline id="main-board-video">
           <source src={mainVideo} type="video/mp4" />
           Your browser does not support HTML5 video.
         </video>
