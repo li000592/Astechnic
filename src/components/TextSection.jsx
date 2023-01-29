c5b7eb35c45bbbe3a6fe64f41de6e01437977d06
li000592 <li000592@algonquinlive.com>
2023-01-28 19:34:42 -0500

    update landing page

--- src/components/TextSection.jsx
+++ src/components/TextSection.jsx
@@ -4,16 +4,22 @@ import "./TextSection.css";
 
 export default function TextSection({ title = "", body = "" }) {
   return (
-    <section>
-      <Grid container>
-        <Grid item xs={12} md={6}>
-          <div className="text-section-container">
-            <h2>{title}</h2>
-            <p>{body}</p>
-          </div>
+    <>
+      <section>
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <div className="text-section-container">
+              <h2>{title}</h2>
+              <p>{body}</p>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={6}></Grid>
         </Grid>
-        <Grid item xs={12} md={6}></Grid>
-      </Grid>
-    </section>
+      </section>
+      <div className="left-brick-container animate__animated animate__fadeInLeft">
+        <div className="left-brick-brick"></div>
+        <div className="left-brick-triangle" />
+      </div>
+    </>
   );
 }
