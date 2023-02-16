1d627b0674b4bcffe95614ab779f6a22c725cf4f
li000592 <li000592@algonquinlive.com>
2023-02-15 22:40:01 -0500

    update 404 page

---
+++ src/pages/error/FOF.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function FOF() {
+  return (
+    <section id="empty-page-frame" style={{padding:"50px 0", minHeight:"60vh", fontWeight:800}}>
+      <h1 style={{fontWeight:800}}>Page not found.</h1>
+      <p style={{fontWeight:800}}>
+        The requested page you are looking for can not be found. Please check the address and try again. If you can not
+        locate the page, please use the menu at the top to navigate the site.
+      </p>
+    </section>
+  );
+}
+
+export default FOF;
