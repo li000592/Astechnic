2757f9871f24df47839e75906f8597c4c40f87a7
li000592 <li000592@algonquinlive.com>
2023-02-15 22:42:06 -0500

    update 404 page

--- src/pages/error/FOF.jsx
+++ src/pages/error/FOF.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function FOF() {
   return (
-    <section id="empty-page-frame" style={{padding:"50px 0", minHeight:"60vh", fontWeight:800}}>
+    <section id="empty-page-frame" style={{padding:"50px 0", minHeight:"70vh", fontWeight:800}}>
       <h1 style={{fontWeight:800}}>Page not found.</h1>
       <p style={{fontWeight:800}}>
         The requested page you are looking for can not be found. Please check the address and try again. If you can not
